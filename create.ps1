eca6998c2747071187f83ae2791c75729d6abcfb
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2021-07-21 21:49:38 +0200

    added .keep add/remove functionality, moved variables to top, comments

--- create.ps1
+++ create.ps1
@@ -1,11 +1,14 @@
 
 
-$dependantModules=@('PSDepends', 'Plaster')
+$DependentModules=@('PSDepend', 'Plaster')
 
-$ModuleName = 'lollercopter'
 $TargetDirectory = 'C:\testoutput\test'
 
+$templatefolder = "$psscriptroot\TemplateFolders"
 
+$Helpermodulesfolder = "$psscriptroot\TemplateFolders\Helpermodules\"
+
+#### Bootstrap required modules for creating the scaffold
 Foreach ($Module in $DependentModules){
     If (-not (Get-Module $module -ListAvailable)){
         Install-Module -name $Module -Scope CurrentUser -Force
@@ -13,16 +16,39 @@ Foreach ($Module in $DependentModules){
     Import-Module $module -ErrorAction Stop
 }
 
-$Helpermodulesfolder = "$psscriptroot\TemplateFolders\Helpermodules\"
+#removing the .keep files used to store the folder structure in git
+$keepfiles = Get-ChildItem $templatefolder -File -Filter '.keep' -Recurse
 
-if ((Get-ChildItem $Helpermodulesfolder -Recurse)) {
-    Remove-Item "$Helpermodulesfolder\*" -Recurse -Force
+if ($keepfiles) {
+    $keepfiles | ForEach-Object{
+        Remove-Item $_.FullName -Force | Out-Null
+    }
 }
+
+######## Clean the helpermodules folder (will become obsolete once PSDepend is part of build)
+if ((Get-ChildItem $Helpermodulesfolder -Recurse)) {
+    Remove-Item "$Helpermodulesfolder\*" -Recurse -Force | Out-Null
+}
+
+##### runPSDepend for getting required modules
 
 Invoke-PSDepend -path "$psscriptroot\Plaster.depend.psd1" -Install -Force -Verbose
 
-Invoke-Plaster -TemplatePath . -DestinationPath $TargetDirectory -Verbose -Name $ModuleName
+#####create scaffold
+Invoke-Plaster -TemplatePath . -DestinationPath $TargetDirectory -Verbose
 
+######## Clean the helpermodules folder (will become obsolete once PSDepend is part of build)
 if ((Get-ChildItem $Helpermodulesfolder -Recurse)) {
-    Remove-Item "$Helpermodulesfolder\*" -Recurse -Force
+    Remove-Item "$Helpermodulesfolder\*" -Recurse -Force | Out-Null
+}
+
+# add .keep files again to templatefolderstructure
+
+$templatefolders = Get-ChildItem $templatefolder -Directory -Recurse
+
+if ($templatefolders) {
+    $templatefolders | ForEach-Object{
+        # $_.FullName
+        New-Item -Path $_.FullName -Name '.keep' -ItemType 'file' -Force | Out-Null
+    }
 }
